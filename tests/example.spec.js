eba5b751fa6f8021b498ef4208ef77f17799e936
Mariia Mikhaleva <marmaroon@yandex.ru>
2022-11-23 23:15:59 +0300

    update

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -3,37 +3,50 @@ const { test, expect } = require('@playwright/test');
 const phoneNumber = '9278626209';
 const userPassword = 'Qwerty123';
 
-
-test.describe('Auth', () =>  {
-  test('should be succesfull fill phone number', async ({ page }) => {
-  // link
+test.beforeEach(async ({page}) => {
   await page.goto('https://samokat2023-test.smartheadtest.ru/');
-  // click 
-  const authButton = page.locator('//*[@id="__next"]/div/div/section/button[2]').click()
-  //
+  await page.getByRole('button', { name: 'Ок' }).click();
+  const authButton = page.getByText('Войти').click()
   const fillMobilePhone = page.getByPlaceholder('+7 9XX XXX-XX-XX');
   // fill number
   await fillMobilePhone.fill(phoneNumber);
   // click button
   await page.getByText('Дальше').click()
 
-  const fillPassword = page.getByPlaceholder('Пароль');
+  const fillPassword = page.getByPlaceholder('Пароль'); //в чем разница если выносить отдельно page или отдельно fillPassword?
   await fillPassword.fill(userPassword);
   // click button
   await page.getByText('Войти').click()
-
-  await page.geНовых карточек пока нет. Чтобы их получить, сделайте заказ в Самокате
-
-  })
+  // скрин страницы лк
+  await page.screenshot({ path: 'img/login.jpg' });
 })
 
+test.describe('Click tabs', () =>  {
+  test('should be succesfull click on tab collections', async ({ page }) => {
+  await page.getByRole('tab', { name: 'Моя коллекция 0' }).click();
+  await page.screenshot({ path: 'img/collection.jpg' })
+  })
 
-// test.beforeEach(async ({ page }) => {
-//   await page.goto('http://localhost:1111/landing');
-//   const authButton = page.locator('//*[@id="__next"]/div/div/section/button[2]').click()
-// });
-// test('homepage has title and links to intro page', async ({ page }) => {
-//   await page.goto('https://playwright.dev/');
+  test('should be successfull click on tab cards and open 1 card',  async ({ page }) => {
+  await page.getByRole('tab', { name: 'Карточки' }).click();
+  await page.screenshot({ path: 'img/cards.jpg' })
+  // await page.getByText('18').click();  получаем количество неоткрытых карточек
+  await page.getByRole('button', { name: 'ПЕРЕВЕРНУТЬ' }).click();
+  await page.screenshot({ path: 'img/opencard.jpg' })
+  // const countUnopenedCards = page.locator('//*[@id="__next"]/div/div[6]/header/div/div/div/div/button[2]/span[2]/span')
+  // expect(countUnopenedCards).toHaveText('1')
+})
+
+test('should be successfull click on tab game and start it',  async ({ page }) => {
+  await page.getByRole('tab', { name: 'Игра' }).click();
+  await page.screenshot({ path: 'img/game.jpg' })
+  // await page.getByText('10').click(); // количество открытых, но непросмотренных карточек
+
+  })
+
+});
+
+
 
 //   // Expect a title "to contain" a substring.
 //   await expect(page).toHaveTitle(/Playwright/);
