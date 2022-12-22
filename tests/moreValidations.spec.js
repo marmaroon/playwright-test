1d58707facb1740b7e89718dfeec02a56b8a459f
marmaroon <mihasm99@mail.ru>
2022-12-22 19:25:31 +0300

    debug

--- tests/moreValidations.spec.js
+++ tests/moreValidations.spec.js
@@ -8,5 +8,14 @@ await page.goto('https://rahulshettyacademy.com/AutomationPractice/');
 await expect(page.locator('#displayed-text')).toBeVisible();
 await page.locator('#hide-textbox').click();
 await expect(page.locator('#displayed-text')).toBeHidden();
+page.on('dialog', dialog => dialog.accept()); //js event, if cancel use dismiss
+await page.locator('#confirmbtn').click();
+
+// hover
+await page.locator('#mousehover').hover();
+
+
+
+
 
 })
