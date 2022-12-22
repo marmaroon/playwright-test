01edbc53397745ecfc727c025c7fa25e7e8ce61c
marmaroon <mihasm99@mail.ru>
2022-12-22 16:43:26 +0300

    add submit order and assertions orderid

--- tests/shoppingtest.spec.js
+++ tests/shoppingtest.spec.js
@@ -1,9 +1,10 @@
 const { test, expect, errors } = require('@playwright/test');
 
-test.only('Client App login', async({page}) =>
+test('Client App login', async({page}) =>
 {
     const products = page.locator('.card-body');
     const productName = 'zara coat 3'
+    const email = 'anshika@gmail.com';
     await page.goto('https://rahulshettyacademy.com/client');
     await page.locator('#userEmail').fill('anshika@gmail.com');
     await page.locator('#userPassword').type('Iamking@000');
@@ -40,7 +41,32 @@ test.only('Client App login', async({page}) =>
         }
         
     }
-    await page.pause()
+    // assert correct email and subnit the order
+    await expect(page.locator('text=anshika@gmail.com')).toHaveText(email);
+    await page.locator('.action__submit').click();
+    // assert text thanks for the order
+    await expect(page.locator('.hero-primary')).toHaveText(' Thankyou for the order. ');
+
+    const orderId = await page.locator('.em-spacer-1 .ng-star-inserted').textContent();
+    console.log(orderId);
+
+    await page.locator("button[routerlink*='myorders']").click();
+    await page.waitForLoadState('networkidle');
+    await page.locator('tbody').waitFor(); // wait for loading selectors
+    
+    const rows = await page.locator('tbody tr');
+    for(let i = 0; i<await rows.count(); ++i) {
+        const rowOrderId = await rows.nth(i).locator('th').textContent();
+        if (orderId.includes(rowOrderId)) {
+            await rows.nth(i).locator('button').first().click();
+            break;
+        }        
+    }
+
+
+    const orderIdDetails = await page.locator('.col-text').textContent();
+    expect(orderId.includes(orderIdDetails)).toBeTruthy();
+
 
 
 
