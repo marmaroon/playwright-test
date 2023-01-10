f38015059bd2f9a9d7846923552c72b7cb9053be
Mariia Mikhaleva <marmaroon@yandex.ru>
2023-01-10 14:38:05 +0300

    refactoring

--- tests/moreValidations.spec.js
+++ tests/moreValidations.spec.js
@@ -1,6 +1,6 @@
 const { test, expect } = require('@playwright/test');
 
-test.only('Popup validations', async({page}) => {
+test('Popup validations', async({page}) => {
 
 await page.goto('https://rahulshettyacademy.com/AutomationPractice/');
 // await page.goto('https://google.com');
@@ -19,10 +19,21 @@ await framesPage.locator('li a[href*="lifetime-access"]:visible').click();
 const textCheck = await framesPage.locator('.text h2').textContent();
 console.log(textCheck.split(' ')[1]);
 
+})
 
+test('Screenshot & Visual validations', async ({page})=>{
+    await page.goto('https://rahulshettyacademy.com/AutomationPractice/');
+    await expect(page.locator('#displayed-text')).toBeVisible();
+    await page.locator('#displayed-text').screenshot({path: 'screenshotlocator.png'})
+    await page.locator('#hide-textbox').click();
+    await page.screenshot({path: 'screenshot.png'});
+    await expect(page.locator('#displayed-text')).toBeHidden();
+    page.on('dialog', dialog => dialog.accept()); //js event, if cancel use dismiss
+    await page.locator('#confirmbtn').click();
+})
 
-
-
-
+test.only('Compare screenshots', async({page}) => {
+    await page.goto('https://flightaware.com/');
+    expect(await page.screenshot()).toMatchSnapshot('landing.png');
 
 })
--- tests/networkTest2.spec.js
+++ tests/networkTest2.spec.js
@@ -1,6 +1,6 @@
 const { test, expect, request, errors } = require('@playwright/test');
 const {APIUtils} = require('./utils/APIUtils');
-const loginPayLoad = {userEmail: "anshika@gmail.com", userPassword: "Iamking@000"};
+const loginPayLoad = {userEmail: "mashamasha@gmail.com", userPassword: "Iamking@00"};
 const orderPayLoad = {orders: [{country: "Cuba", productOrderedId: "6262e95ae26b7e1a10e89bf0"}]};
 const fakePayLoadOrders = {data: [], message: "No Orders"};
 let response;
@@ -18,22 +18,14 @@ test('Place the order', async({page}) =>
         window.localStorage.setItem('token',value);
     }, response.token ); // the first is func and second is a parameter
     await page.goto('https://rahulshettyacademy.com/client/');
-    await page.route("https://rahulshettyacademy.com/api/ecom/order/get-orders-for-customer/62026f4edfa52b09e0a20b18",
-async route=>
-{
-  const response =  await page.request.fetch(route.request());
-  let body =fakePayLoadOrders;
-   route.fulfill(
-      {
-        response,
-        body,
+    await page.locator("button[routerlink*='myorders']").click();
 
-      });
-    //intercepting response - APi response->{ playwright fakeresponse}->browser->render data on front end
-});
+    await page.route('https://rahulshettyacademy.com/api/ecom/order/get-orders-details?id=63bc2fb2568c3e9fb1f1dc3e',
+    route=> route.continue({url: 'https://rahulshettyacademy.com/api/ecom/order/get-orders-details?id=63bc284a568c3e9fb1f1d3e2'})
+    )
 
-await page.locator("button[routerlink*='myorders']").click();
-//await page.pause();
-console.log(await page.locator(".mt-4").textContent());
+    await page.locator('text=View').first().click();
+    await page.pause()
+
 
 });
