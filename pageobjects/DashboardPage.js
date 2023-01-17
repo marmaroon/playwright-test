84132e4acc9e0b0a6af694d52392d493b52b6de5
Mariia Mikhaleva <marmaroon@yandex.ru>
2023-01-17 22:54:26 +0300

    pageobjects pattern fix

--- pageobjects/DashboardPage.js
+++ pageobjects/DashboardPage.js
@@ -1,31 +1,42 @@
-class DashboardPage {
+class DashboardPage
+{
+constructor(page)
+{
+    this.page = page;
+    this.products = page.locator(".card-body");
+    this.productsText = page.locator(".card-body b");
+    this.cart =  page.locator("[routerlink*='cart']");
+    this.orders = page.locator("button[routerlink*='myorders']");
 
-    constructor(page)
-    {
-        this.products = page.locator('.card-body');
-        this.productsText = page.locator('.card-body b');
-        this.cart = page.locator('[routerlink*="cart"]');
-    }
+}
 
-    async searchProductAddCart(productName)
+async searchProductAddCart(productName)
+{
+   
+    const titles= await this.productsText.allTextContents();
+    console.log(titles);
+    const count = await this.products.count();
+    for(let i =0; i < count; ++i)
     {
-        const titles = await this.productsText.allTextContents();
-        console.log(titles);
-        
-        const countProducts = await this.products.count();  //how much elements in this selector
-        for (let i = 0; i < countProducts; ++i) {
-            if(await this.products.nth(i).locator('b').textContent() === productName) {
-                // add to cart
-                await this.products.nth(i).locator('text= Add To Cart').click();
-                break;
-            }
-        }
+    if(await this.products.nth(i).locator("b").textContent() === productName)
+    {
+        //add to cart
+        await this.products.nth(i).locator("text= Add To Cart").click();
+        break;
+     }
     }
+}
 
-    async navigateToCart()
-    {
-        await this.cart.click();
-    }
+async navigateToOrders()
+{
+    await this.orders.click();
+}
+
+
+async navigateToCart()
+{
+    await this.cart.click();
+}
 
 }
 module.exports = {DashboardPage};
